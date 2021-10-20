633f0d853c8bdf86473d8a09a28670b62ff40f1c
robwhite <robert.white3120@gmail.com>
2021-10-20 09:35:08 -0500

    setup project

---
+++ server.js
@@ -0,0 +1,58 @@
+//___________________
+//Dependencies
+//___________________
+const express = require('express');
+const methodOverride  = require('method-override');
+const mongoose = require ('mongoose');
+const app = express ();
+const db = mongoose.connection;
+require('dotenv').config()
+//___________________
+//Port
+//___________________
+// Allow use of Heroku's port or your own local port, depending on the environment
+const PORT = process.env.PORT || 3003;
+
+//___________________
+//Database
+//___________________
+// How to connect to the database either via heroku or locally
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Connect to Mongo &
+// Fix Depreciation Warnings from Mongoose
+// May or may not need these depending on your Mongoose version
+mongoose.connect(MONGODB_URI);
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+//___________________
+//Middleware
+//___________________
+
+//use public folder for static assets
+app.use(express.static('public'));
+
+// populates req.body with parsed info from forms - if no data from forms will return an empty object {}
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON - may or may not need it depending on your project
+
+//use method override
+app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
+
+
+//___________________
+// Routes
+//___________________
+//localhost:3000
+app.get('/' , (req, res) => {
+  res.send('Hello World!');
+});
+
+//___________________
+//Listener
+//___________________
+app.listen(PORT, () => console.log( 'Listening on port:', PORT));
